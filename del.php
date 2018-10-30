4855b915fc34079fc5e74827093ec033f0e2069c
marethaayuf <marethaayuf@gmail.com>
2018-10-30 10:00:30 +0700

    action

--- del.php
+++ del.php
@@ -5,7 +5,7 @@ if (mysqli_connect_errno()){
 	echo "Koneksi database gagal : " . mysqli_connect_error();
 }
 $Id = $_GET['Id'];
-$sql_delete=("DELETE FROM users WHERE Id='$Id' ");
+$sql_delete=("DELETE FROM user WHERE Id='$Id' ");
 mysqli_query($conn,$sql_delete)or die(mysqli_error($conn));
 echo '<script>
 alert("Data Berhasil Dihapus");
--- editan.php
+++ editan.php
@@ -9,7 +9,7 @@ $Username = $_POST['Username'];
 $Password = $_POST['Password'];
 $Level = $_POST['Level'];
 $Fullname = $_POST['Fullname'];
-$sql_update=("UPDATE users SET Username='$Username', Password='$Password', Level='$Level', Fullname='$Fullname' WHERE Id='$Id'");
+$sql_update=("UPDATE user SET Username='$Username', Password='$Password', Level='$Level', Fullname='$Fullname' WHERE Id='$Id'");
 mysqli_query($conn,$sql_update)or die(mysqli_error($conn));
 echo '<script>
 alert("Data Berhasil Diedit");
